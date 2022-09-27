8c36f5dc86659ab6496175744655bf14b9889f3a
Saunak Sen <sen@uthsc.edu>
2022-09-27 09:37:49 -0500

    Command line for running slider server.

---
+++ runsliderserver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+__usage="
+Usage: $(basename $0) [PORT] [IPADDR]\n
+\n
+Options:\n
+  PORT\t      Port in which to run Pluto Slider Server.\n
+  IPADDR\t    IP address of server\n
+"
+
+if [[ ( $@ == "--help" ) || ( $@ == "-h" ) ]]
+then
+	echo -e ${__usage}
+	exit 0
+fi
+
+command="
+import PlutoSliderServer;
+
+PlutoSliderServer.run_directory(\"notebooks/\", SliderServer_port=${1},SliderServer_host=\"${2}\")
+"
+echo ${command}
+julia --optimize=0 -e "${command}"
